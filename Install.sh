4ace6ff82373c8ca3d86da901977b96d06e74d75
iluvpidgeons <gamernoahaza@gmail.com>
2026-09-23 13:34:46 -0400

    update

--- Install.sh
+++ Install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # script made for ubuntu 26.04.1 on harmony laptops
-# should install vlc, audacity, gimp, canva start menu shortcut, soundshow, and davinci resolve
-# add option to unpin Help and App Center and pin Terminal, SoundShow, Gimp, Audacity, and Canva
-# add option to install ublock origin on firefox
+# this script does not configure the network
 
+echo "Updating & Upgrading System"
+sleep 1
 sudo apt update -y && sudo apt upgrade -y && sudo snap refresh
 
-# non-required utilites
 echo
-echo "Installing utilities"
+echo "Installing Utilities"
 sleep 1
 sudo apt install -y \
     curl \
@@ -17,7 +16,7 @@ sudo apt install -y \
     glmark2
 
 echo
-echo "Installing apps"
+echo "Installing Apps"
 sleep 1
 sudo apt install -y \
     audacity \
@@ -27,17 +26,17 @@ sudo apt install -y \
 echo
 echo "Installing Soundshow & Canva"
 sleep 1
-wget -P /tmp https://github.com/soundshow-app/soundshow-downloads/releases/download/v2026.05.06/SoundShow-linux-x64.zip
-unzip /tmp/SoundShow-linux-x64.zip -d /opt
-chmod +x /opt/SoundShow/SoundShow.x86_64
-wget -P /usr/share/icons/ https://images.seeklogo.com/logo-png/65/1/canva-logo-png_seeklogo-653227.png
-wget -P /usr/share/icons/ https://raw.githubusercontent.com/iluvpidgeons/AutoinstallUbuntu/refs/heads/main/SoundShow.png
+sudo wget -P /tmp https://github.com/soundshow-app/soundshow-downloads/releases/download/v2026.05.06/SoundShow-linux-x64.zip
+sudo unzip /tmp/SoundShow-linux-x64.zip -d /opt
+sudo chmod +x /opt/SoundShow/SoundShow.x86_64
+sudo wget -P /usr/share/icons/ https://images.seeklogo.com/logo-png/65/1/canva-logo-png_seeklogo-653227.png
+sudo wget -P /usr/share/icons/ https://raw.githubusercontent.com/iluvpidgeons/AutoinstallUbuntu/refs/heads/main/SoundShow.png
 
 CanvaDesktopFile="/usr/share/applications/canva.desktop"
 SoundShowDesktopFile="/usr/share/applications/soundshow.desktop"
-# find different directory for icons
 CanvaLogo="/usr/share/icons/canva-logo-png_seeklogo-653227.png"
 SoundShowLogo="/usr/share/icons/SoundShow.png"
+SoundShowExec="/opt/SoundShow/SoundShow.x86_64"
 
 cat > "$CanvaDesktopFile" <<EOF
 [Desktop Entry]
@@ -57,7 +56,7 @@ Version=1.0
 Type=Application
 Name=SoundShow
 Comment=Open SoundShow
-Exec=/opt/SoundShow/SoundShow.x86_64
+Exec=$SoundShowExec
 Icon=$SoundShowLogo
 Terminal=false
 Categories=Audio;
@@ -68,14 +67,18 @@ chmod +x $SoundShowDesktopFile
 
 gsettings set org.gnome.shell favorite-apps \
 "[
-  'firefox.desktop',
+  'firefox_firefox.desktop',
   'org.gnome.Nautilus.desktop',
-  'org.gnome.Terminal.desktop',
+  'org.gnome.Ptyxis.desktop',
   'gimp.desktop',
   'audacity.desktop',
   'soundshow.desktop',
   'canva.desktop'
 ]"
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface clock-format '12h'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
 
 # read -r -p "Failed to download Davinci Resolve, would you like to open their website? [y/N]: " answer
 # case "$answer" in
--- readme.md
+++ readme.md
@@ -1,2 +1,17 @@
-# TODO
-add network to `autoinstall.yaml`
+# Harmony Laptop Ubuntu Install Script
+Use the `Install.sh` bash script or `user-data` autoinstall file on a Ubuntu Laptop or Desktop
+
+Installs
+* VLC
+* Audacity
+* GIMP
+* Canva (Shortcut)
+* SoundShow
+
+Configures
+* Dark Mode
+* Time AM/PM
+* Dock Autohide
+* Dock Placement (Bottom)
+* Configues Pins
+* Network (user-data only)
